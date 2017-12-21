3b54466e9458dc407861db16df7785f841f328da
K9WTHOZWIR0R5SW\Administrator <1181141792@qq.com>
2017-12-21 13:59:59 +0800

    账号登录

--- src/page/mall/login/AccountLogin.vue
+++ src/page/mall/login/AccountLogin.vue
@@ -1,6 +1,14 @@
 <template>
-  <div class="mall-wrapper">
-    账号登录
+  <div class="account-login">
+    <input type="text" id="phone">
+    <input type="text" id="password">
+    <div>请输入正确的手机号</div>
+    <input type="button" value="确认登录">
+    <div>
+      <span>忘记密码</span>
+      <span>|</span>
+      <span>验证登录</span>
+    </div>
   </div>
 </template>
 <script>
@@ -8,3 +16,8 @@
     name: "AccountLogin"
   }
 </script>
+<style lang="stylus" scoped>
+  .account-login{
+    padding :0.7rem 0.55rem 0 0.55rem;
+  }
+</style>
