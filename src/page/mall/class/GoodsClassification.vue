2a8eea9fb73718d11e8dda1c9f5ebd8f573ae396
K9WTHOZWIR0R5SW\Administrator <1181141792@qq.com>
2017-12-28 19:38:12 +0800

    商品分类列表

--- src/page/mall/class/GoodsClassification.vue
+++ src/page/mall/class/GoodsClassification.vue
@@ -11,67 +11,57 @@
     <div class="class-area">
       <div class="class-area-l">
         <ul ref="classpart">
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>手机数码</li>
-          <li>电脑办公</li>
+          <li v-for="(item,index) in goodClassList" :key="index" :class="{calssActive:selected==index}" @click="changeClass(index)">{{item.gcName}}</li>
         </ul>
       </div>
-      <div class="class-area-r">
-        <div class="con-class"  ref="conClass">
-          <div class="class-tit">手机</div>
-          <ul>
-            <li>商务手机</li>
-            <li>商务手机</li>
-            <li>商务手机</li>
-            <li>商务手机</li>
-            <li>商务手机</li>
-            <li>儿童手机</li>
-          </ul>
+      <div class="class-area-r"  ref="conClass">
+        <div class="con-class"  v-for="(items,index) in goodClassList" :key="index" v-if="showIndex===index">
+          <div  v-for="(item,index) in items.classList" :key="index">
+            <div class="class-tit">{{item.gcName}}</div>
+            <ul>
+              <li  v-for="(obj,index) in item.classList" :key="index">{{obj.gcName}}</li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
+    <footer-tab :category="1"></footer-tab>
   </div>
-  
 </template>
 <script>
+  document.title="分类";
+  import FooterTab from "../../../components/mall/FooterTab.vue";
   export  default {
+    data:function () {
+      return{
+        goodClassList:'',
+        selected:0,
+        showIndex:0
+      }
+    },
+    components:{
+      FooterTab,
+    },
     mounted () {
       this.axios.get(this.baseURL.mall+"/m/search/goodsClass",).then(res =>{
         console.log(res);
+        if(res.data.h.code==200){
+          this.goodClassList=res.data.b.goodsClass;
+        }
       });
       let searchH=window.getComputedStyle(this.$refs.search).height.replace("px","");
       console.log(searchH);
       let winH = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
       console.log(winH);
-      let classH=winH-searchH;
+      let classH=winH-searchH-50;
       this.$refs.classpart.style.height = classH +'px';
       this.$refs.conClass.style.height = classH +'px';
+    },
+    methods:{
+      changeClass(index){
+        this.selected = index;
+        this.showIndex = index;
+      }
     }
   }
 </script>
@@ -126,22 +116,25 @@
       float left;
       ul{
         overflow-y scroll;
+        background #f0f0f5;
         li{
           width 100%;
           height .45rem;
           line-height .45rem;
           border-bottom 1px solid #e5e5e5;
-          background #f0f0f5;
           font-size .12rem;
           color #666;
           text-align center;
+          padding 0 .1rem;
+          box-sizing border-box;
+          overflow hidden;
         }
       }
       ul::-webkit-scrollbar {/*隐藏滚轮*/
         display: none;
         width: 0px;
       }
-      ul li:first-child{
+      ul li.calssActive{
         background #fff;
         color #ff8800;
       }
@@ -150,10 +143,10 @@
       }
     }
     .class-area-r{
-      width 100%;
       margin-left .95rem;
+      overflow-y scroll;
       .con-class{
-        overflow-y scroll;
+      
       }
       .con-class::-webkit-scrollbar {/*隐藏滚轮*/
         display: none;
