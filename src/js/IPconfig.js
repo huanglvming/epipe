226960dce699897cddbff8726780b8f9a65ff067
huanglvming <237737971@qq.com>
2018-01-12 18:05:44 +0800

    2018-1-12/代码更新

--- src/js/IPconfig.js
+++ src/js/IPconfig.js
@@ -2,4 +2,4 @@ export const baseURL = {
   mall: "http://192.168.3.111/epmall-front",  //梁焱升本地
   //  mall: "http://192.168.3.167:8888",             //167测试环境
 };
-export const mallToken = "f02675251cc54e7b91f4d193778e6f10";
+export const mallToken = "5123ca2f140142419a584120177e83bf";
