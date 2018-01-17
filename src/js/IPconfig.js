97a76c4ac85e6ad1089f1025610b75244859337b
huanglvming <237737971@qq.com>
2018-01-17 09:30:22 +0800

    修改设置与获取商城token

--- src/js/IPconfig.js
+++ src/js/IPconfig.js
@@ -2,10 +2,6 @@ export const baseURL = {
   // mall: "http://192.168.3.111/epmall-front",  //梁焱升本地
    mall: "http://192.168.3.167:8888",             //167测试环境
 };
-// function fn() {
-//   const token = localStorage.getItem("token");
-//   return token;
-// }
 export const mallToken = {
   setToken: function(token){
     localStorage.setItem("token",token);
