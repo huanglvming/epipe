296e68e54e730abd6249cc37a261f0dc957f5e4b
K9WTHOZWIR0R5SW\Administrator <1181141792@qq.com>
2018-01-18 17:32:25 +0800

    支付

--- src/page/mall/shop/ConfirmOrder.vue
+++ src/page/mall/shop/ConfirmOrder.vue
@@ -107,6 +107,38 @@
           invoiceId:this.invoiceId
         })).then(res=>{
           console.log(res);
+          if(res.data.h.code==200){
+            let payInfoList=res.data.b;
+            function onBridgeReady(){
+              WeixinJSBridge.invoke(
+                'getBrandWCPayRequest', {
+                  "appId":payInfoList.appId,     //公众号名称，由商户传入
+                  "timeStamp":payInfoList.timeStamp,         //时间戳，自1970年以来的秒数
+                  "nonceStr":payInfoList.nonceStr, //随机串
+                  "package":payInfoList.package,
+                  "signType":payInfoList.signType,         //微信签名方式：
+                  "paySign":payInfoList.paySign //微信签名
+                },
+                function(res){
+                  if(res.err_msg == "get_brand_wcpay_request:ok" ) {
+                    
+                  }     // 使用以上方式判断前端返回,微信团队郑重提示：res.err_msg将在用户支付成功后返回    ok，但并不保证它绝对可靠。
+                }
+              );
+            }
+            if (typeof WeixinJSBridge == "undefined"){
+              if( document.addEventListener ){
+                document.addEventListener('WeixinJSBridgeReady', onBridgeReady, false);
+              }else if (document.attachEvent){
+                document.attachEvent('WeixinJSBridgeReady', onBridgeReady);
+                document.attachEvent('onWeixinJSBridgeReady', onBridgeReady);
+              }
+            }else{
+              onBridgeReady();
+            }
+          }else{
+            this.$toast(res.data.h.msg);
+          }
         })
       }
     },
